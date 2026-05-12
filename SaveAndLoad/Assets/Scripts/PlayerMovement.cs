a5606df3617ae0122fc356eeda2b1d03d57d36e4
supersticker <163869848+supersticker@users.noreply.github.com>
2026-05-12 13:34:42 +0200

    commit

--- SaveAndLoad/Assets/Scripts/PlayerMovement.cs
+++ SaveAndLoad/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,27 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D playerRb;
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float moveSpeed = 10f;
+    float actualSpeed;
 
     Vector2 moveInput;
-
+    private void Start()
+    {
+        actualSpeed = moveSpeed;
+    }
     public void OnMove(InputAction.CallbackContext value)
     {
         moveInput = value.ReadValue<Vector2>();
     }
 
+    private void Update()
+    {
+        actualSpeed = Mathf.MoveTowards(actualSpeed, moveSpeed, Time.deltaTime * 2);
+    }
+
     void FixedUpdate()
     {
-        playerRb.linearVelocity = moveInput * moveSpeed;
+        playerRb.linearVelocity = moveInput * actualSpeed;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -23,6 +32,7 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("Coin"))
         {
             GameManager.AddScore();
+            actualSpeed += 2f;
 
             Destroy(collision.gameObject);
         }
